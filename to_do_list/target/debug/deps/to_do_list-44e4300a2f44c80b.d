44e9ca510e21b77ebdebe493b7fab111e6833459
retrymp3 <retrymp492@gmail.com>
2024-06-06 17:59:59 +0530

    Removing target folder

--- to_do_list/target/debug/deps/to_do_list-44e4300a2f44c80b.d
+++
@@ -1,5 +0,0 @@
-/home/retry/Documents/rust/ToDoList-In-Rust/to_do_list/target/debug/deps/to_do_list-44e4300a2f44c80b: src/main.rs
-
-/home/retry/Documents/rust/ToDoList-In-Rust/to_do_list/target/debug/deps/to_do_list-44e4300a2f44c80b.d: src/main.rs
-
-src/main.rs:
