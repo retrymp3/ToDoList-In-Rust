3c93be12c0509e29a8b82823554fd9906353483e
retrymp3 <retrymp492@gmail.com>
2024-06-06 18:08:01 +0530

    Handling target dir

---
+++ to_do_list/target/debug/deps/to_do_list-44e4300a2f44c80b.d
@@ -0,0 +1,5 @@
+/home/retry/Documents/rust/ToDoList-In-Rust/to_do_list/target/debug/deps/to_do_list-44e4300a2f44c80b: src/main.rs
+
+/home/retry/Documents/rust/ToDoList-In-Rust/to_do_list/target/debug/deps/to_do_list-44e4300a2f44c80b.d: src/main.rs
+
+src/main.rs:
